6108480cd00a489a97895fbf3e66ab1d68a5e3bd
Hosward Villoria <hosward.avc@gmail.com>
2018-11-25 16:53:10 -0300

    devise token auth routes and inclusion in controller made

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope module: 'api' do
+    resources :movies, only: [:index, :create, :update]
+  end
+
+  resources :people, only: [:index, :create, :update]
 end
